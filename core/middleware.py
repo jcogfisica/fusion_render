30336a2cbf8c9c9d5814699065a333746f0ab56c
jcogfisica <jcogfisica@yahoo.com.br>
2025-08-16 16:55:40 -0300

    Projeto Fusion 11

--- core/middleware.py
+++ core/middleware.py
@@ -1,10 +1,11 @@
 # core/middleware.py
-class DebugMiddleware:
+class DebugAfterRenderMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        response = self.get_response(request)  # Primeiro deixa o Django gerar a response
         if request.path == "/":  # Só dispara para a homepage
-            raise Exception("Debug: Página index acessada no Render")  # <-- força erro
-        response = self.get_response(request)
+            # Garante que só vai lançar o erro depois de processar a view/template
+            raise Exception("Debug: Página index já renderizada no Render")
         return response
